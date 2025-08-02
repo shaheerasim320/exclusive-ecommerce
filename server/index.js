3c8ba7ecdbd0e57eea1b3cc29d28699736faf514
Shaheer <sp23-bcs-063@cuilahore.edu.pk>
2025-08-02 22:56:50 +0500

    Testing

--- server/index.js
+++ server/index.js
@@ -30,84 +30,88 @@ dotenv.config();
 const PORT = process.env.PORT || 8080
 const app = express()
 
-app.use(express.json());
+// app.use(express.json());
 
-app.use(cookieParser())
+// app.use(cookieParser())
 
-app.use(cors({
-    origin: true,
-    credentials: true
-}))
+// app.use(cors({
+//     origin: true,
+//     credentials: true
+// }))
 
-app.get("/api/cron/guest-cleanup", async (req, res) => {
-    try {
-        await cleanGuestData();
-        res.status(200).json({ message: "Guest data cleanup complete" });
-    } catch (error) {
-        console.error("Guest cleanup error:", error);
-        res.status(500).json({ error: "Guest cleanup failed" });
-    }
-});
+// app.get("/api/cron/guest-cleanup", async (req, res) => {
+//     try {
+//         await cleanGuestData();
+//         res.status(200).json({ message: "Guest data cleanup complete" });
+//     } catch (error) {
+//         console.error("Guest cleanup error:", error);
+//         res.status(500).json({ error: "Guest cleanup failed" });
+//     }
+// });
 
-app.get("/api/cron/billing-cleanup", async (req, res) => {
-    try {
-        await cleanupBillings();
-        res.status(200).json({ message: "Billing cleanup complete" });
-    } catch (error) {
-        console.error("Billing cleanup error:", error);
-        res.status(500).json({ error: "Billing cleanup failed" });
-    }
-});
+// app.get("/api/cron/billing-cleanup", async (req, res) => {
+//     try {
+//         await cleanupBillings();
+//         res.status(200).json({ message: "Billing cleanup complete" });
+//     } catch (error) {
+//         console.error("Billing cleanup error:", error);
+//         res.status(500).json({ error: "Billing cleanup failed" });
+//     }
+// });
 
-app.get("/api/cron/extend-flash-sales", async (req, res) => {
-    try {
-        await extendOrRecycleFlashSales();
-        res.status(200).json({ message: "Extend flash sales cron job complete" });
-    } catch (error) {
-        console.error("Flash sales extension error:", error);
-        res.status(500).json({ error: "Flash sales extension failed" });
+// app.get("/api/cron/extend-flash-sales", async (req, res) => {
+//     try {
+//         await extendOrRecycleFlashSales();
+//         res.status(200).json({ message: "Extend flash sales cron job complete" });
+//     } catch (error) {
+//         console.error("Flash sales extension error:", error);
+//         res.status(500).json({ error: "Flash sales extension failed" });
 
-    }
-});
+//     }
+// });
 
-mongoose.connect(process.env.MONGODB_URI, { dbName: "exclusive-ecommerce" })
-    .then(conn => console.log(`MongoDB Connected With Server: ${conn.connection.host}`))
-    .catch(err => console.log(`Error Occured: ${err}`))
+// mongoose.connect(process.env.MONGODB_URI, { dbName: "exclusive-ecommerce" })
+//     .then(conn => console.log(`MongoDB Connected With Server: ${conn.connection.host}`))
+//     .catch(err => console.log(`Error Occured: ${err}`))
 
-app.use("/api/v1/products", productRoutes)
+// app.use("/api/v1/products", productRoutes)
 
-app.use("/api/v1/users", userRoutes)
+// app.use("/api/v1/users", userRoutes)
 
-app.use("/api/v1/cart", assignGuestId, cartRoutes)
+// app.use("/api/v1/cart", assignGuestId, cartRoutes)
 
-app.use("/api/v1/coupons", couponRoutes)
+// app.use("/api/v1/coupons", couponRoutes)
 
-app.use("/api/v1/wishlist", assignGuestId, wishlistRoutes)
+// app.use("/api/v1/wishlist", assignGuestId, wishlistRoutes)
 
-app.use("/api/v1/image", imageRoutes);
+// app.use("/api/v1/image", imageRoutes);
 
-app.use("/api/v1/billing", verifyAccessToken, billingRoutes)
+// app.use("/api/v1/billing", verifyAccessToken, billingRoutes)
 
-app.use("/api/v1/payment", verifyAccessToken, paymentRoutes)
+// app.use("/api/v1/payment", verifyAccessToken, paymentRoutes)
 
-app.use("/api/v1/orders", verifyAccessToken, orderRoutes)
+// app.use("/api/v1/orders", verifyAccessToken, orderRoutes)
 
-app.use("/api/v1/card", verifyAccessToken, cardRoutes)
+// app.use("/api/v1/card", verifyAccessToken, cardRoutes)
 
-app.use("/api/v1/address", verifyAccessToken, addressRoutes)
+// app.use("/api/v1/address", verifyAccessToken, addressRoutes)
 
-app.use("/api/v1/category", categoryRoutes)
+// app.use("/api/v1/category", categoryRoutes)
 
-app.use("/api/v1/admin", verifyAccessToken, verifyAdmin, adminRoutes)
+// app.use("/api/v1/admin", verifyAccessToken, verifyAdmin, adminRoutes)
 
-app.use("/api/v1/flashSale", flashSaleRoutes)
+// app.use("/api/v1/flashSale", flashSaleRoutes)
 
 
-app.use("/", (req, res) => {
-    res.status(200).json({message:"Welcome To Exclusive Ecommerce"})
-})
-app.use((req, res) => {
-    res.status(404).json({ message: "Route not found" });
+// app.use("/", (req, res) => {
+//     res.status(200).json({message:"Welcome To Exclusive Ecommerce"})
+// })
+// app.use((req, res) => {
+//     res.status(404).json({ message: "Route not found" });
+// });
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Test Successful" });
 });
 
 export default serverless(app);
